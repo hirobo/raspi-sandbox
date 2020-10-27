c57983aeca1869e8f8cfd39e4c6019876a47d03c
hirobo <hiroko.hirobo@gmail.com>
2020-10-27 09:48:41 +0100

    first commit and add led.py

---
+++ led.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+PNO = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PNO, GPIO.OUT)
+
+for i in range(10):
+    print("i=",i)
+    GPIO.output(PNO, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(PNO, GPIO.LOW)
+    time.sleep(0.5)
+
+GPIO.cleanup()
